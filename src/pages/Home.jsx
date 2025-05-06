b3830b8d970a5a0a3a8b0e8e9349fe98d33974ea
Ariful <arifulmit17@gmail.com>
2025-05-06 22:58:44 +0600

    added faq section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import hbo from '../assets/hbo.png'
 import Tsports from '../assets/TSports.png'
 import Youtube from '../assets/youtube.jpg'
 
-import  { useRef, useState } from "react";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -57,13 +57,35 @@ const Home = () => {
         </SwiperSlide>
       </Swiper>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            <div className='w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
             {
                 data.map(item=><Item key={item.id} item={item}></Item> )
             }
             </Suspense>
             
+        </div>
+        <div>
+            <section className='w-11/12 mt-10 mx-auto '>
+                <h1 className='text-center text-4xl font-bold'>F.A.Q</h1>
+                <div>
+                <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+  <input type="radio" name="my-accordion-2" defaultChecked />
+  <div className="collapse-title font-semibold">How do I create an account?</div>
+  <div className="collapse-content text-sm">Click the "Register" button in the top  and follow the registration process.</div>
+</div>
+<div className="collapse collapse-arrow bg-base-100 border border-base-300">
+  <input type="radio" name="my-accordion-2" />
+  <div className="collapse-title font-semibold">I forgot my password. What should I do?</div>
+  <div className="collapse-content text-sm">Click on "Forgot Password" on the login page and follow the instructions sent to your email.</div>
+</div>
+<div className="collapse collapse-arrow bg-base-100 border border-base-300">
+  <input type="radio" name="my-accordion-2" />
+  <div className="collapse-title font-semibold">How do I update my profile information?</div>
+  <div className="collapse-content text-sm">Go to profile page and give new information and click update to make changes.</div>
+</div>
+                </div>
+            </section>
         </div>
         </div>
         
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -2,13 +2,35 @@ import React, { use } from 'react';
 import { AuthContext } from '../Contexts/AuthContext';
 
 const UserProfile = () => {
-    const {user}=use(AuthContext)
+    const {user,setUser,updateUser}=use(AuthContext)
+    const handleUpdate=e=>{
+        e.preventDefault();
+        const name=e.target.name.value;
+        const photo=e.target.photo.value;
+        console.log(name,photo);
+        updateUser({displayName: name, photoURL : photo}).then(()=>{
+            setUser({...user,displayName: name, photoURL : photo})
+          }).catch(error=>{
+            console.log(error);
+            setUser(user)
+          })
+    }
     return (
-        <div className='flex'>
+        <div className='flex gap-20'>
             <img src={user.photoURL} alt="" />
             <div>
             <h1>User name: {user.displayName}</h1>
             <h1>Email : {user.email}</h1>
+            </div>
+            <div className='flex flex-col gap-5'>
+                <h1>Update user profile name and picture</h1>
+                <form onSubmit={handleUpdate}>
+                <input className='bg-white m-3 p-3 rounded-2xl'  type="text" placeholder='Username' name='name' />
+                <br />
+                <input className='bg-white m-3 p-3 rounded-2xl'  type="url" placeholder='PhotoUrl' name='photo' />
+                <br />
+                <button className='btn m-3 btn-primary' type='submit'>Update</button>
+                </form>
             </div>
             
             
